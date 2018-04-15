dcb8fad5a1be6c3697561a5e7da5d1c8c755767d
GovindKalyanaMOJ <govindaraju.kalyanaraman@digital.justice.gov.uk>
2018-04-15 20:41:36 +0100

    checkbox and radio button improved

--- ui/src/app/components/person/create/create.component.ts
+++ ui/src/app/components/person/create/create.component.ts
@@ -2,7 +2,7 @@ import { CommonData } from './../../../staticdata/common-data';
 import { PersonService } from './../../../services/person.service';
 
 import { Component } from '@angular/core';
-import { Person }  from './../person'
+import { Person } from './../person'
 
 @Component({
   selector: 'app-create',
@@ -10,30 +10,67 @@ import { Person }  from './../person'
   styleUrls: ['./create.component.css']
 })
 export class CreatePersonComponent {
-
-  constructor(private personService : PersonService){}
-
+  constructor(private personService: PersonService) { }
   staticData = new CommonData();
   person = new Person();
 
-  title : String;
-  foreName : String;
-  surname : String;
-
-  registerPerson(){
-    this.personService.registerPerson(this.person).subscribe(res => console.log('person component ',res));
+  registerPerson() {
+    this.personService.registerPerson(this.person).subscribe(res => console.log('person component ', res));
   }
 
-  updateDisability(value,event) {
+  updateDisability(value, event) {
 
-    if(event.target.checked) {
+    if (event.target.checked) {
       this.person.disabilityOptions.push(value);
     } else {
       var index = this.person.disabilityOptions.indexOf(value);
-      if(index > -1){
-        this.person.disabilityOptions.splice(index,1);
+      if (index > -1) {
+        this.person.disabilityOptions.splice(index, 1);
       }
     }
   }
 
+  updateExistingClient(event) {
+    if (event.target.checked) {
+      this.person.existingClient = "Y";
+    } else {
+      this.person.existingClient = "NÍ";
+    }
+  }
+
+  updateRequestSpecificSolicitor(event) {
+    if (event.target.checked) {
+      this.person.requestSpecificSolicitor = "Y";
+    } else {
+      this.person.requestSpecificSolicitor = "N";
+    }
+  }
+
+  updatePreviousConviction(event) {
+    if (event.target.checked) {
+      this.person.previousConviction = "Y";
+    } else {
+      this.person.previousConviction = "N";
+    }
+  }
+
+  updateConflictCheck(event) {
+    if (event.target.checked) {
+      this.person.conflictCheck = "Y";
+    } else {
+      this.person.conflictCheck = "N";
+    }
+  }
+
+  updateRiskAssessmentDone(event) {
+    if (event.target.checked) {
+      this.person.riskAssessmentDone = "Y";
+    } else {
+      this.person.riskAssessmentDone = "N";
+    }
+  }
+
+  updateRiskAssessmentType(value, event) {
+    this.person.riskAssessmentType = event.target.value;
+  }
 }
